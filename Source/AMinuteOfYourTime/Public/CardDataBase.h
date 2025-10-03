6305d445746230f8bd9767aa5b75f26088fb7e95
Jake Morgenstern <jmorgenstern15@gmail.com>
2025-10-03 16:27:38 -0400

    Added generic card handler interface so that each handler can handle their own drop/removal behavior

--- Source/AMinuteOfYourTime/Public/CardDataBase.h
+++ Source/AMinuteOfYourTime/Public/CardDataBase.h
@@ -29,7 +29,7 @@ public:
 	int32 ActionPointOffset;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 ActionPointMultiplier;
+	int32 ActionPointMultiplier = 1;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FText CardName;
